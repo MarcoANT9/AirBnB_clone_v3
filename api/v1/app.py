d12cf512b6e8378e2b43b44a79ae3019cf3b333f
Luz <zulsb2093@gmail.com>
2020-04-28 04:50:44 +0000

    Add module to show error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,6 +8,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
@@ -15,6 +16,11 @@ def tear_down(self):
     """Close session"""
     storage.close()
 
+
+@app.errorhandler(404)
+def not_found(error):
+    """Return JSON wiht error 404"""
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 if __name__ == "__main__":
     app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
