306f5cc3bc00bc33b1223c090de73adb3b37007d
Luz <zulsb2093@gmail.com>
2020-04-28 06:21:38 +0000

    Add fix import

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """API for registering blueprint and starting flask"""
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
