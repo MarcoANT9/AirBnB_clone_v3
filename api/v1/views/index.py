2c25fbd46cf1253c31b57f02180375ac62a428d1
Luz <zulsb2093@gmail.com>
2020-04-28 04:01:03 +0000

    Add modules to show some status.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,17 @@ from models import storage
 def status():
     """Returns a json of status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def storage_numbers():
+    """Return the numbers of all classes in storage"""
+    cls_numbers = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(cls_numbers)
