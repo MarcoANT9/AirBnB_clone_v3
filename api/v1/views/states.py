433289c6fdad25c318590b6ea5f3b61c5d7d797c
MarcoANT9 <acalde27@hotmail.com>
2020-04-29 01:52:49 +0000

    Added states.py in api/v1/views

---
+++ api/v1/views/states.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""Index page for flask that displays status and stats"""
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models.state import State
+
+
+@app_views.route("/states/", methods=['GET'])
+def get_all_states():
+    """ This function retrieves all states. Has no parameters. """
+    states_dict = []
+    """
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+    """
+    all_object_dict = storage.all(State).values()
+    for value in all_object_dict:
+        states_dict.append(value.to_dict())
+    return jsonify(states_dict)
+
+
+@app_views.route("/states/<state_id>", methods=['GET'])
+def get_one_state(state_id):
+    """ This function retrieves one state given an id.
+        state_id → Id of the requested state.
+    """
+    storage.reload()
+    object_dict = storage.get(State, state_id)
+    if object_dict:
+        return jsonify(object_dict.to_dict())
+    abort(404)
+
+
+@app_views.route("/states/<state_id>", methods=['DELETE'])
+def delete_an_state(state_id):
+    """ This function retrieves one state given an id and
+        deletes it.
+        state_id → Id of state to delete.
+        returns an empty dictionary on success.
+        raises a 404 error if state doesn't exists.
+    """
+    object_dict = storage.get(State, state_id)
+    if object_dict:
+        storage.delete(object_dict)
+        storage.save()
+        storage.reload()
+        return jsonify({})
+    abort(404)
+
+@app_views.route("/states/", methods=['POST'])
+def create_state():
+    """ This function creates a new state. """
+    try:
+        new_state = request.get_json()
+        if new_state:
+            if "name" not in new_state:
+                return jsonify("Missing name"), 400
+            state = State()
+            state.name = new_state['name']
+            storage.save()
+            return jsonify(state.to_dict()), 201
+    except:
+        return jsonify("Not a JSON"), 400
+
+
+@app_views.route("/states/<state_id>", methods=['PUT'])
+def update_state(state_id):
+    """ This function updates a state. """
+    try:
+        state_update = request.get_json()
+    except:
+        return jsonify("Not a JSON"), 400
+
+    if state_update:
+        object_ = storage.get(State, state_id)
+        if object_:
+            ignored_attr = ["id", "created_at", "updated_at"]
+            for key in state_update:
+                if key not in ignored_attr:
+                    value = state_update[key]
+                    print (object_.__dict__[key])
+                    object_.__dict__[key] = value
+                    print (object_.__dict__[key])
+
+            object_.save()
+            print(storage.get(State, state_id).to_dict())
+            print(object_.to_dict())
+            return jsonify(object_.to_dict())
+        abort(404)
