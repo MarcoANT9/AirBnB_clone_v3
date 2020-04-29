afec1e2e0ecbddacea6d2c09a9a4d439a5872962
Luz <zulsb2093@gmail.com>
2020-04-29 21:50:31 +0000

    Add more imports

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -3,4 +3,5 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
+from models.place import Place
 from models.amenity import Amenity
