17917fcd6b1d163e678de903757c7b34d6227f74
Luz <zulsb2093@gmail.com>
2020-04-29 03:55:50 +0000

    Remove some comments

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,9 +7,9 @@ app_views = Blueprint("my_blueprint", __name__, url_prefix="/api/v1")
 """Import flask views"""
 from api.v1.views.index import *
 from api.v1.views.states import *
-"""
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
+"""
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.users import *
