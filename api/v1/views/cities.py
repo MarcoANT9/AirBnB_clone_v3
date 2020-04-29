d32d55506e52250b66233d57e5a6eac0d59b1afc
Luz <zulsb2093@gmail.com>
2020-04-29 21:39:11 +0000

    Add imports

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -3,6 +3,7 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
+from models.state import State
 from models.city import City
 
 
