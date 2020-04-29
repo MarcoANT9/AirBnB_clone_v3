d62932419f3f33666d0a3c4bcf84fe7a9d96e3f9
Luz <zulsb2093@gmail.com>
2020-04-29 21:58:21 +0000

    Add a method to try

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -5,3 +5,13 @@ from flask import jsonify, abort, request, make_response
 from models import storage
 from models.place import Place
 from models.amenity import Amenity
+
+@app_views.route("/places/<place_id>/amenities", methods=['GET'], strict_slashes=False)
+def get_amenities_by_places(place_id):
+    list_pl_a = []
+    place = storage.get("Place", place_id)
+    if not place:
+        abort(404)
+    for place in place.amenities:
+        list_pl_a.append(place.to_dict())
+    return jsonify(list_pl_a)
