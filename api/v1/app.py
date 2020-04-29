77ba3c2370022ab10b1281a94705bc65695d3a16
MarcoANT9 <acalde27@hotmail.com>
2020-04-29 06:05:25 +0000

    app.py: fixed resourses in cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
-cors = CORS(app, resourses={r"/api/*": {"origins": "*"}})
+cors = CORS(app, resourses={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
