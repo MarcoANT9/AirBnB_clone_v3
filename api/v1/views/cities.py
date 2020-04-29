d2c155e974a7456b2540725a162b5dd5284b49e1
Luz <zulsb2093@gmail.com>
2020-04-29 21:35:46 +0000

    Removing unnecessary imports

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -3,7 +3,6 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
-from models.state import State
 from models.city import City
 
 
