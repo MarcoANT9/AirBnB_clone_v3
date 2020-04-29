1afa930b4259a2803ca9b2a46e01efccea679747
MarcoANT9 <acalde27@hotmail.com>
2020-04-29 05:22:42 +0000

    Cities: changed variable names

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -15,7 +15,7 @@ def get_all_cities_in_state(state_id):
     """
     all_cities = []
     state = storage.get("State", state_id)
-    if not state:
+    if state is None:
         abort(404)
     all_object_dict = storage.all(City).values()
     for value in all_object_dict:
@@ -29,10 +29,12 @@ def get_a_city(city_id):
     """ Retrieves a city given an id.
         city_id → Id of the requested city.
     """
-    all_cities = storage.all(City).values()
-    for value in all_cities:
-        if value.id == city_id:
-            return jsonify(value.to_dict())
+    city_dict = storage.get(City, city_id)
+    if city_dict is not None:
+        all_cities = storage.all(City).values()
+        for city in all_cities:
+            if city.id == city_id:
+                return jsonify(city_dict.to_dict())
     abort(404)
 
 
@@ -60,13 +62,13 @@ def create_city(state_id):
         state_id → state in which the new city will be created.
     """
     new_city = request.get_json()
-    if not new_city:
+    if new_city is None:
         abort(400, "Not a JSON")
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-    if new_state:
-        if "name" not in new_state:
+    if new_city:
+        if "name" not in new_city:
             abort(400, "Missing name")
         new_city['state_id'] = state_id
         city = City(**new_city)
@@ -76,16 +78,16 @@ def create_city(state_id):
 
 
 @app_views.route("/cities/<city_id>", methods=['PUT'], strict_slashes=False)
-def update_city(state_id):
+def update_city(city_id):
     """ This function updates a city. """
     city_update = request.get_json()
-    if not city_update:
+    if city_update is None:
         abort(400, "Not a JSON")
 
     object_ = storage.get(City, city_id)
     if object_:
         ignored_attr = ["id", "created_at", "updated_at", "state_id"]
-        for key, value in state_update.items():
+        for key, value in city_update.items():
             if key not in ignored_attr:
                 setattr(object_, key, value)
             object_.save()
